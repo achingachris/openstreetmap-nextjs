2cc5698c1b854548e47695aa95209b726d52b261
chrisachinga <achinga.chris@gmail.com>
2022-04-22 16:23:40 +0300

    clean

--- component/OpenStreetMap.js
+++ component/OpenStreetMap.js
@@ -1,24 +1,41 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import React, { useState, useRef } from 'react'
+import { MapContainer, TileLayer, Marker } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
 
-const OpenStreetMap = () => {
-  const position = [51.505, -0.09]
+const Map2 = () => {
+  const [center, setCenter] = useState({ lat: -4.043477, lng: 39.668205 })
+  const ZOOM_LEVEL = 9
+  const mapRef = useRef()
+
   return (
-    <div className='container'>
-      <MapContainer
-        center={[-4.05746, 39.66394]}
-        zoom={13}
-        scrollWheelZoom={false}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-        />
-        <Marker position={[-4.05746, 39.66394]}>
-          <Popup>I am Here</Popup>
-        </Marker>
-      </MapContainer>
-    </div>
+    <>
+      <div className='container'>
+        <div className='container'>
+          <h1 className='text-center-mt-5'>OpenStreetMap Embeded</h1>
+        </div>
+        <div className='row'>
+          <div className='col'>
+            <div className='container'>
+              <MapContainer center={center} zoom={ZOOM_LEVEL} ref={mapRef}>
+                <TileLayer
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                  url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                />
+                {location.loaded && !location.error && (
+                  <Marker
+                    position={[
+                      location.coordinates.lat,
+                      location.coordinates.lng,
+                    ]}
+                  ></Marker>
+                )}
+              </MapContainer>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
-export default OpenStreetMap
+export default Map2
