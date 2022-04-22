b494b498b9da76cc605e31f83ea37d407f2195e3
chrisachinga <achinga.chris@gmail.com>
2022-04-22 16:18:57 +0300

    clean

--- component/Map2.js
+++ component/Map2.js
@@ -1,37 +1,11 @@
 import React, { useState, useRef } from 'react'
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-import L from 'leaflet'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import { MapContainer, TileLayer, Marker } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
-import osm from './osm-providers'
-import useGeoLocation from './useGeoLocation'
-import { map } from 'leaflet'
-
-const markerIcon = new L.Icon({
-  iconUrl: require('../public/marker.png'),
-  iconSize: [40, 40],
-  iconAnchor: [17, 46], //[left/right, top/bottom]
-  popupAnchor: [0, -46], //[left/right, top/bottom]
-})
 
 const Map2 = () => {
   const [center, setCenter] = useState({ lat: -4.043477, lng: 39.668205 })
   const ZOOM_LEVEL = 9
   const mapRef = useRef()
-  const location = useGeoLocation()
-
-  const showMyLocation = () => {
-    if (location.loaded && !location.error) {
-      mapRef.current.LeafletElement.flyTo(
-        [location.coordinates.lat, location.coordinates.lng],
-        ZOOM_LEVEL,
-        { animate: true }
-      )
-    } else {
-      alert(location.error.message)
-    }
-  }
 
   return (
     <>
@@ -44,8 +18,8 @@ const Map2 = () => {
             <div className='container'>
               <MapContainer center={center} zoom={ZOOM_LEVEL} ref={mapRef}>
                 <TileLayer
-                  url={osm.maptiler.url}
-                  attribution={osm.maptiler.attribution}
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                  url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
                 />
                 {location.loaded && !location.error && (
                   <Marker
@@ -60,16 +34,6 @@ const Map2 = () => {
           </div>
         </div>
       </div>
-
-      {/* <div className='container'>
-        <div className='row my-4'>
-          <div className='col d-flex justify-content-center'>
-            <button className='btn btn-primary' onClick={showMyLocation}>
-              Locate Me <FontAwesomeIcon icon='globe' />
-            </button>
-          </div>
-        </div>
-      </div> */}
     </>
   )
 }
